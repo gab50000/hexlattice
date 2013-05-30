5d8791f0641a122710377be2a1bf61c4336669e4
gab50000 <fake@xyz.de>
2013-05-31 01:37:41 +0200

    fitting radius to width and height of screen

--- lattice.py
+++ lattice.py
@@ -24,21 +24,22 @@ class hex_lattice:
 		for i in enumerate(self.nodes):
 			for j in enumerate(i[1]):
 				if j[1]==0:
-					pygame.draw.polygon(self.window, (200,200,200), hexagon((size[0]/2+j[0]*self.latticevec[0][0]+i[0]*self.latticevec[1][0], size[1]/2+j[0]*self.latticevec[0][1]+i[0]*self.latticevec[1][1]), self.radius), 1)
+					pygame.draw.polygon(self.window, (200,200,200), hexagon((j[0]*self.latticevec[0][0]+i[0]*self.latticevec[1][0], size[1]/2+j[0]*self.latticevec[0][1]+i[0]*self.latticevec[1][1]), self.radius), 1)
 				else:
-					pygame.draw.polygon(self.window, (0,0,0), hexagon((size[0]/2+j[0]*self.latticevec[0][0]+i[0]*self.latticevec[1][0], size[1]/2+j[0]*self.latticevec[0][1]+i[0]*self.latticevec[1][1]), self.radius), 0)
+					pygame.draw.polygon(self.window, (0,0,0), hexagon((j[0]*self.latticevec[0][0]+i[0]*self.latticevec[1][0], size[1]/2+j[0]*self.latticevec[0][1]+i[0]*self.latticevec[1][1]), self.radius), 0)
 
 
-size=(400,300)			
+size=(800,600)			
 pygame.init()
 fps=pygame.time.Clock()
-window=pygame.display.set_mode((800,600))
-
-hexlatt=hex_lattice(50, 50, 5, window, size)
-
+window=pygame.display.set_mode(size)
+(hex_lines, hex_columns)=(10, 10)
+hexlatt=hex_lattice(hex_lines, hex_columns, min(int(float(size[0])/(hex_lines+hex_columns-1)/2), int(float(size[1])/(hex_lines+hex_columns))) , window, size)
+#~ hexlatt=hex_lattice(5,3,20, window, size)
+#~ hex1=hexagon(size, 20)
 while 1:
 	window.fill(pygame.Color(255,255,255))
-	#~ pygame.draw.polygon(window,(0,0,0), hex1, 10)
+	#~ pygame.draw.polygon(window,(0,0,0), hex1, 2)
 	hexlatt.draw()
 	pygame.display.update()
 	fps.tick(30)
