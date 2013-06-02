3bbc5bd66424ca7aa588cf127c98329afd3be0a2
gab50000 <fake@xyz.de>
2013-06-03 01:09:05 +0200

    move works, no interaction yet, row and column indices wrong?

--- lattice.py
+++ lattice.py
@@ -13,23 +13,34 @@ def hexagon(center, radius):
 
 class particle:
 	#direction: 0 up, clockwise up to 5
-	def __init__(self, position, direction):
+	def __init__(self, position, direction, columns, rows):
 		self.position=position
 		self.direction=direction
+		self.columns=columns
+		self.rows=rows
 	def move(self):
-		if direction==0:
-			position[0]-=1
-			position[1]+=1	
-		elif direction==1:
-			position[1]+=1
-		elif direction==2:
-			position[0]+=1
-		elif direction==3:
-			position[0]+=1
-			position[1]-=1
-		elif direction==4:
-			position[1]-=1
-		else: position[0]-=1
+		if self.direction==0:
+			self.position[0]-=1
+			self.position[1]+=1	
+		elif self.direction==1:
+			self.position[1]+=1
+		elif self.direction==2:
+			self.position[0]+=1
+		elif self.direction==3:
+			self.position[0]+=1
+			self.position[1]-=1
+		elif self.direction==4:
+			self.position[1]-=1
+		else: self.position[0]-=1
+		
+		if self.position[0]<0:
+			self.position[0]+=self.rows
+		if self.position[1]<0:
+			self.position[1]+=self.columns
+		if self.position[0]>=self.rows:
+			self.position[0]-=self.rows
+		if self.position[1]>=self.columns:
+			self.position[1]-=self.columns
 		
 class hex_lattice:
 	def __init__(self,rows, columns, r, particles, window, size):
@@ -44,8 +55,8 @@ class hex_lattice:
 		self.window=window
 		self.size=size
 		self.particles=[]
-		self.place_particles(self.particles)
-		# self.init_particles
+		#self.place_particles(self.particles)
+		self.init_particles(particles)
 		
 	def place_particles(self, particles):
 		while particles > 0:
@@ -59,31 +70,31 @@ class hex_lattice:
 		positions=range(self.rows*self.columns)
 		random.shuffle(positions)
 		for i in range(p_nr):
-			self.particles.append(particle((positions[i]/self.columns, positions[i]%rows), random.randint(0,5)))
+			self.particles.append(particle([positions[i]/self.columns, positions[i]%self.rows], random.randint(0,5), self.rows, self.columns))
 		
 	def move(self):
 		for particle in self.particles:
 			particle.move()
-		self.check_collision()
+		#self.check_collision()
 
 
 	
-	def check_collision(self):
+	#def check_collision(self):
 
 		
 	def draw(self):
-		for i in enumerate(self.nodes):
-			for j in enumerate(i[1]):
-				if j[1]==1:
-					pygame.draw.polygon(self.window, (80,80,80), hexagon(((self.rows+self.columns-1)*self.radius/4.+j[0]*self.latticevec[0][0]+i[0]*self.latticevec[1][0]+1, size[1]/2+j[0]*self.latticevec[0][1]+i[0]*self.latticevec[1][1]), self.radius-1), 0)
-				pygame.draw.polygon(self.window, (200,200,200), hexagon(((self.rows+self.columns-1)*self.radius/4.+j[0]*self.latticevec[0][0]+i[0]*self.latticevec[1][0], size[1]/2+j[0]*self.latticevec[0][1]+i[0]*self.latticevec[1][1]), self.radius), 2)
+		for p in self.particles:
+			pygame.draw.polygon(self.window, (80,80,80), hexagon(((self.rows+self.columns-1)*self.radius/4.+p.position[1]*self.latticevec[0][0]+p.position[0]*self.latticevec[1][0]+1, size[1]/2+p.position[1]*self.latticevec[0][1]+p.position[0]*self.latticevec[1][1]), self.radius-1), 0)
+		for i in range(self.rows):
+			for j in range(self.columns):
+				pygame.draw.polygon(self.window, (200,200,200), hexagon(((self.rows+self.columns-1)*self.radius/4.+j*self.latticevec[0][0]+i*self.latticevec[1][0], size[1]/2+j*self.latticevec[0][1]+i*self.latticevec[1][1]), self.radius), 2)
 
 
 size=(800,600)			
 pygame.init()
 fps=pygame.time.Clock()
 window=pygame.display.set_mode(size)
-(hex_lines, hex_columns)=(10, 10)
+(hex_lines, hex_columns)=(30, 40)
 hexlatt=hex_lattice(hex_lines, hex_columns, min(int(float(size[0])/(hex_lines+hex_columns-1)/2), int(float(size[1])/(hex_lines+hex_columns))), 50 , window, size)
 #~ hexlatt=hex_lattice(5,3,20, window, size)
 #~ hex1=hexagon(size, 20)
@@ -92,4 +103,5 @@ while 1:
 	#~ pygame.draw.polygon(window,(0,0,0), hex1, 2)
 	hexlatt.draw()
 	pygame.display.update()
-	fps.tick(3)
+	hexlatt.move()
+	fps.tick(10)
